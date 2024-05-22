a0efbba3533e5d706523566728e275c31e95b681
Łukasz Pawela <lukasz.pawela@gmail.com>
2024-05-22 17:24:06 +0200

    reformat

--- test/MPS_search.jl
+++ test/MPS_search.jl
@@ -9,8 +9,8 @@
     max_states = 20000
     to_show = length(expected_energies)
 
-    β = 2.
-    dβ = β/8.0
+    β = 2.0
+    dβ = β / 8.0
 
     Dcut = 32
     var_ϵ = 1E-8
@@ -18,33 +18,15 @@
 
     @testset "without any purification" begin
         igp = prune(ig)
-        schedule = fill(dβ, Int(ceil(β/dβ)))
+        schedule = fill(dβ, Int(ceil(β / dβ)))
         ψ = MPS(igp, Dcut, var_ϵ, max_sweeps, schedule)
         states, lprob, _ = solve(ψ, max_states)
         @test sort(energy(states[1:to_show], igp)) ≈ expected_energies
     end
 
     @testset "with purification" begin
-        sol_log = low_energy_spectrum(
-            ig,
-            Dcut,
-            var_ϵ,
-            max_sweeps,
-            dβ,
-            β,
-            :log,
-            max_states
-        )
-        sol_lin = low_energy_spectrum(
-            ig,
-            Dcut,
-            var_ϵ,
-            max_sweeps,
-            dβ,
-            β,
-            :lin,
-            max_states
-        )
+        sol_log = low_energy_spectrum(ig, Dcut, var_ϵ, max_sweeps, dβ, β, :log, max_states)
+        sol_lin = low_energy_spectrum(ig, Dcut, var_ϵ, max_sweeps, dβ, β, :lin, max_states)
         @test sol_log.energies[1:to_show] ≈ sol_lin.energies[1:to_show]
         @test sol_log.energies[1:to_show] ≈ expected_energies[1:to_show]
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,10 +4,7 @@ using SpinGlassNetworks
 using MetaGraphs
 using LinearAlgebra
 
-my_tests = [
-    "MPS_search.jl",
-    "ising_MPS.jl",
-]
+my_tests = ["MPS_search.jl", "ising_MPS.jl"]
 
 for my_test in my_tests
     include(my_test)
